2f6584f4fe6c8b350ddd18b2eb34e4bec85c952d
Brittany Richards <brittany.m.richards3@gmail.com>
2021-09-23 09:53:13 -0500

    created partials directory with separate views

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -1,15 +1,35 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
 
 @Controller
 public class RollDiceController {
 
-    @GetMapping("/roll-dice");
+    @GetMapping("/roll-dice")
 
     public String rollDice(){
         return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+
+    public String diceGuess(
+            @PathVariable int n, Model model) {
+
+        Random randomNum = new Random();
+        int random = randomNum.nextInt((6 - 1) + 1) + 1;
+
+        model.addAttribute("diceRoll", random);
+        model.addAttribute("userGuess", n);
+        model.addAttribute("isCorrect", random == n);
+
+        return "roll-dice";
+
     }
 
 }
