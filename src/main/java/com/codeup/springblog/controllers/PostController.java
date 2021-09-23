e503043175dac5b914f816121615541ece9e4fac
Brittany Richards <brittany.m.richards3@gmail.com>
2021-09-23 10:17:46 -0500

    added show, index

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,24 +1,38 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
     @GetMapping(path = "/posts")
-    @ResponseBody
 
-    public String postsIndex() {
-        return "Posts index page";
+    public String postsIndex(Model model) {
+
+        List<Post> allPosts = new ArrayList<>();
+
+        allPosts.add(new Post("Another Post!", "Another one"));
+        allPosts.add(new Post("Yet Another Post!", "Yet another one"));
+
+        model.addAttribute("posts", allPosts);
+
+        return "post/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
 
-    public String singlePost(
-            @PathVariable int id
-    ) {
-        return "Viewing post " + id + ".";
+    public String singlePost(@PathVariable int id, Model model) {
+
+        Post post = new Post("New Post", "New Body");
+        model.addAttribute("postID", id);
+        model.addAttribute("newPost", post);
+
+        return "post/show";
     }
 
     @GetMapping("/posts/create")
