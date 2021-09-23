c5b71f0f2cd2a5c44a931032e4a86180e0f83b4d
Brittany Richards <brittany.m.richards3@gmail.com>
2021-09-23 15:07:56 -0500

    JPA exercise started

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,7 +1,18 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 200)
     private String title;
+
+    @Column(nullable = false)
     private String body;
 
     public Post(String title, String body) {
